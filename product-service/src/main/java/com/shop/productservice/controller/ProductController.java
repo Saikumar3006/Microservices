de6e5ab6de9d22c54b10ad88649d7d812b6de585
Sai kumar <saikumarchandupatla@Sais-MacBook-Air.local>
2026-04-05 23:22:28 -0700

    Created Product Service API

---
+++ product-service/src/main/java/com/shop/productservice/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.shop.productservice.controller;
+
+import com.shop.productservice.dto.ProductRequest;
+import com.shop.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+
+    }
+
+
+}
---
+++ product-service/src/main/java/com/shop/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.shop.productservice.service;
+
+import com.shop.productservice.dto.ProductRequest;
+import com.shop.productservice.model.Product;
+import com.shop.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {} has been created", product.getId());
+    }
+}
